a3069a7f5d1c7c3b6ef900b0354fa3790d675ee0
JuanPabloSE <juancontas@protonmail.com>
2025-10-28 19:43:39 -0300

    Exercicio COMMIT

---
+++ src/bree1038.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class bree1038 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // VARIÁVEIS E ENTRADA
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+        double total;
+
+        // ESTRUTURA CONDICIONAL
+        if (codigo == 1){
+            total = quantidade * 4.00;
+        }
+        else if (codigo == 2){
+            total = quantidade * 4.50;
+        }
+        else if (codigo == 3){
+            total = quantidade * 5.00;
+        }
+        else if (codigo == 4){
+            total = quantidade * 2.00;
+        }
+        else {
+            total = quantidade * 1.50;
+        }
+
+        System.out.printf("Total: R$ %.2f\n", total);
+
+        sc.close();
+    }
+}
