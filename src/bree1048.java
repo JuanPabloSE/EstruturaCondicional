ae2cc127c6e67ecceac8eace06b6ae7d3810dd73
JuanPabloSE <juancontas@protonmail.com>
2025-10-28 21:34:34 -0300

    Exercicio COMMIT

---
+++ src/bree1048.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class bree1048 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // VARIÁVEL E ENTRADA - SALÁRIO
+        double salario = sc.nextDouble();
+        double salarioAumento, reajuste;
+        int percentual;
+
+        // ESTRUTURA CONDICIONAL
+        if (salario <= 400.00){
+            percentual = 15;
+        }
+        else if (salario <= 800.00){
+            percentual = 12;
+        }
+
+        else if (salario <= 1200.00){
+            percentual = 10;
+        }
+
+        else if (salario <= 2000.00){
+            percentual = 7;
+        }
+        else {
+            percentual = 4;
+        }
+
+        reajuste = salario * percentual / 100;
+        salarioAumento = salario + reajuste;
+
+        System.out.printf("Novo salario: %.2f\n", salarioAumento);
+        System.out.printf("Reajuste ganho: %.2f\n", reajuste);
+        System.out.printf("Em percentual: %d %%\n", percentual);
+
+        sc.close();
+    }
+}
