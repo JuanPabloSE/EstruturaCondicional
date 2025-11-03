060b0fc7f4e1bdcd936debd0e0bdd750e09df153
JuanPabloSE <juancontas@protonmail.com>
2025-11-03 11:15:59 -0300

    Exercicio COMMIT

--- src/Exemp11.java
+++ src/Exemp11.java
@@ -6,7 +6,30 @@ public class Exemp11 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        // ENTRADA E VÁRIAVEIS
+        String cargo;
+        double salario;
+        cargo = sc.nextLine();
+        salario = sc.nextDouble();
 
+        // ESTRUTURA CONDICIONAL
+        if (cargo.equalsIgnoreCase("Gerente")){
+            salario += salario * 10 / 100;
+        }
+        else if (cargo.equalsIgnoreCase("Supervisor")){
+            salario += salario * 7 / 100;
+        }
+        else if (cargo.equalsIgnoreCase("Operador")){
+            salario += salario * 5 / 100;
+        }
+        else if (cargo.equalsIgnoreCase("Outros")){
+            salario += salario * 3 / 100;
+        }
+        else {
+            System.out.println("Cargo inválido");
+        }
+
+        System.out.printf("Novo salario: R$ %.2f\n", salario); // SAÍDA DE DADOS DO RESULTADO
 
         sc.close();
     }
