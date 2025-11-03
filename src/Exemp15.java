2274a1bbee2f2674d802de2c21b0f627e900a668
JuanPabloSE <juancontas@protonmail.com>
2025-11-03 20:20:02 -0300

    Exercicio COMMIT

---
+++ src/Exemp15.java
@@ -0,0 +1,53 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exemp15 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int consumo = sc.nextInt();
+        String opcao = sc.next();
+        double valorPagar;
+
+        //Switch Case
+        switch (opcao.toUpperCase()) {
+            case "R":
+                if (consumo <= 500) {
+                    valorPagar = consumo * 0.40;
+                    System.out.printf("Valor a pagar: R$ %.2f\n", valorPagar);
+                } else {
+                    valorPagar = consumo * 0.65;
+                    System.out.printf("Valor a pagar: R$ %.2f\n", valorPagar);
+                }
+                break;
+
+            case "C":
+                if (consumo <= 1000) {
+                    valorPagar = consumo * 0.55;
+                    System.out.printf("Valor a pagar: R$ %.2f\n", valorPagar);
+                } else {
+                    valorPagar = consumo * 0.60;
+                    System.out.printf("Valor a pagar: R$ %.2f\n", valorPagar);
+                }
+                break;
+
+            case "I":
+                if (consumo <= 5000) {
+                    valorPagar = consumo * 0.55;
+                    System.out.printf("Valor a pagar: R$ %.2f\n", valorPagar);
+                }
+                else {
+                    valorPagar = consumo * 0.60;
+                    System.out.printf("Valor a pagar: R$ %.2f\n", valorPagar);
+                }
+                break;
+
+            default:
+                System.out.println("Tipo de instalação inválida. Use R, C ou I.");
+
+        }
+
+        sc.close();
+    }
+}
