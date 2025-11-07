7912462158d6bb56a08f74221212791bb8649564
JuanPabloSE <juancontas@protonmail.com>
2025-11-07 13:14:09 -0300

    Exercicio COMMIT

---
+++ src/bree1059.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class bree1059 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        int i = 1;
+
+        while (i >= 1 && i < 100) {
+            if (i % 2 == 0)
+            System.out.println(i);
+            i++;
+        }
+        }
+    }
+
