a642c1ed0521d68e86175316a1f9e74e2e4ba7a0
JuanPabloSE <juancontas@protonmail.com>
2025-11-08 14:43:11 -0300

    Exercicio COMMIT

---
+++ src/bree1070.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class bree1070 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int contador = 1;
+
+        if (X % 2 == 0){
+            X++;
+        }
+
+        while (contador <= 6) {
+            if (X % 2 != 0){
+                System.out.println(X);
+                X += 2;
+            }
+            contador++;
+        }
+
+
+
+
+        sc.close();
+    }
+}
